f682e120ef69e6d25f1df40b6442444f6df433f7
Ay-tsuisaku <knight214kazanari@gmail.com>
2025-11-13 15:22:40 +0800

    Add files via upload

---
+++ phsensor_final.ino
@@ -0,0 +1,55 @@
+#define pH_SENSOR_PIN 8 // 定義GPIO腳位為GPIO34
+#define VREF 3.3 // ESP32輸入電壓為3.3V
+#define SCOUNT 30 // 取樣數
+int analogBuffer[SCOUNT];
+int analogBufferIndex = 0;
+float averageVoltage = 0;
+float pHValue = 0;
+float pHslope = -4.8475;
+float pHintercept = 18.30;
+  
+void setup()
+{
+  Serial.begin(115200);
+  pinMode(pH_SENSOR_PIN, INPUT);
+}
+  
+float lastpHValue = -1; // 定義最後的pH值為無效(-1)
+  
+void loop()
+{
+  // pH取樣
+  static unsigned long analogSampleTimepoint = 0;
+  if(millis() - analogSampleTimepoint > 40)
+  {
+    analogSampleTimepoint = millis();
+    analogBuffer[analogBufferIndex] = analogRead(pH_SENSOR_PIN);
+    analogBufferIndex++;
+    if(analogBufferIndex == SCOUNT)
+    { 
+      analogBufferIndex = 0;
+    }
+  }   
+    
+  // pH計算
+  static unsigned long printTimepoint = 0;
+  if(millis() - printTimepoint > 800)
+  {
+    printTimepoint = millis();
+    float sum = 0;
+    for(int i = 0; i < SCOUNT; i++)
+    {
+      sum += analogBuffer[i];
+    }
+    averageVoltage = (sum / SCOUNT) * (float)VREF / 4095; // 轉換為電壓值，ESP32的ADC解析度是12位(4095)。
+  
+    pHValue = averageVoltage * pHslope + pHintercept;
+      
+    if (pHValue != lastpHValue) // 如果pH數值有更新，才會將結果Print出來。
+    {
+      lastpHValue = pHValue;
+      Serial.print("pH Value: ");
+      Serial.println(pHValue, 2);
+    }
+  }
+}
